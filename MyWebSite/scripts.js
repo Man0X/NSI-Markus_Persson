cb24ccd2df599deb92ba9c8f722c33d26b7e1b77
Man0X <manoah.avril@gmail.com>
2023-11-14 10:13:32 +0100

    Adding js

---
+++ MyWebSite/scripts.js
@@ -0,0 +1,6 @@
+let kingMiniAnchorDef = document.getElementById("kingMiniAnchorDef")
+let kingMiniAnchor = document.getElementById("kingMiniAnchor")
+
+kingMiniAnchor.addEventListener("click" , function (e) {
+    kingMiniAnchorDef.classList.toggle("invisible")
+});
